0f0a7a7e3be5f3742cf20a52d80e99731c170ece
Newland-A <ajn252@gmail.com>
2020-11-20 08:51:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Code your solutions in this file
 const names = [ 'Lisa', 'Kaitlin', 'Jan' ]
+let event = 'suprise'
 
-function writeCards(names) {
+function writeCards(names, event) {
   let thankYouMessages = []
   for (let i = 0; i < names.length; i++) {
-    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
 
   return thankYouMessages;
@@ -21,12 +22,26 @@ function writeCards(names) {
 //   return thankYouMessages;
 // }
 // writeCards(names)
-let beginningNumber = 10;
+// or while loop
 
-function countDown( beginningNumber ) {
-  while ( beginningNumber > 0 ) {
-    console.log(beginningNumber);
-    beginningNumber -= 1;
+// let beginningNumber = 10;
+
+// function countDown( beginningNumber ) {
+//   while ( beginningNumber > 0 ) {
+//     console.log(beginningNumber);
+//     beginningNumber -= 1;
+//   }
+//   console.log( beginningNumber );
+// }
+
+let number = 10;
+
+function countDown(number) {
+  while (number > 0 ) {
+    console.log(number);
+    number -= 1
   }
-  console.log( beginningNumber );
+  console.log(number);
 }
+
+countDown(number)
